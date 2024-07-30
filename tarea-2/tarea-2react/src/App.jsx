8b756605c6574f0c3c1f8b11766250944a19cd82
carlossanchez <caredusanchez@hotmail.com>
2024-07-30 18:03:55 -0500

    tarea2

---
+++ tarea-2/tarea-2react/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>BATMAN</h1>
+      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fbatman.fandom.com%2Fes%2Fwiki%2FBruce_Wayne_%2528Nueva_Tierra%2529&psig=AOvVaw1XNMsP-OXt5_uTbabItxPi&ust=1722465591398000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCIDb-bTsz4cDFQAAAAAdAAAAABAE" alt="Foto personaje" />
+      <p>
+      Batman es la identidad secreta de Bruce Wayne, un empresario multimillonario, galán y filántropo. Presenció el asesinato de sus padres cuando era niño lo marcó profundamente y lo llevó a entrenarse en la perfección física e intelectual para ponerse un disfraz de murciélago con el fin de combatir el crimen.
+      </p>
+    </div>
+  );
+}
+
+export default App;
